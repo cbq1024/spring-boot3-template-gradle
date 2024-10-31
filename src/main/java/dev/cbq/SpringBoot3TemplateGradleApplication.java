2f179d9665cacc748f02ccadc26c11a814892348
CaoBaoQi <caobq1024@gmail.com>
2024-10-31 14:37:35 +0800

    :tada: feat(core): init commit

---
+++ src/main/java/dev/cbq/SpringBoot3TemplateGradleApplication.java
@@ -0,0 +1,13 @@
+package dev.cbq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3TemplateGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3TemplateGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cbq/SpringBoot3TemplateGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cbq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3TemplateGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
